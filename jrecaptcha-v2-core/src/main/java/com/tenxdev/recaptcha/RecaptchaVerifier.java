cb6c6e85a61c1adeaef01f2770883f9848b2489d
tony.benbrahim <tony.benbrahim@airliquide.com>
2016-09-28 08:51:25 -0500

    Added license header

--- jrecaptcha-v2-core/src/main/java/com/tenxdev/recaptcha/RecaptchaVerifier.java
+++ jrecaptcha-v2-core/src/main/java/com/tenxdev/recaptcha/RecaptchaVerifier.java
@@ -1,6 +1,22 @@
-/**
- * 
- */
+/*
+Copyright 2016 Tony BenBrahim <tony.benbrahim@10xdev.com>
+
+This file is part of jrecaptcha-v2-core.
+
+    jrecaptcha-v2-core is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    jrecaptcha-v2-core is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package com.tenxdev.recaptcha;
 
 import java.io.IOException;
--- jrecaptcha-v2-sample/src/main/java/com/tenxdev/SampleServlet.java
+++ jrecaptcha-v2-sample/src/main/java/com/tenxdev/SampleServlet.java
@@ -1,3 +1,21 @@
+/*
+Copyright 2016 Tony BenBrahim <tony.benbrahim@10xdev.com>
+
+This file is part of jrecaptcha-v2-sample.
+
+    jrecaptcha-v2-sample is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    jrecaptcha-v2-sample is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+*/
 package com.tenxdev;
 
 import java.io.IOException;
